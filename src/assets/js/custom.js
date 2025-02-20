4b639a1207fe82e085bc23e4cbf69b44fb34d1ab
churchilldotexe <churchillexe@gmail.com>
2025-02-20 21:16:01 +0800

    feat: accordion in section are at mobile to tablet view

--- src/assets/js/custom.js
+++ src/assets/js/custom.js
@@ -1,9 +1,25 @@
 // FAQ
 
 const faqItems = Array.from(document.querySelectorAll('.cs-faq-item'));
-        for (const item of faqItems) {
-            const onClick = () => {
-            item.classList.toggle('active')
+for (const item of faqItems) {
+        const onClick = () => {
+                item.classList.toggle('active')
         }
         item.addEventListener('click', onClick)
-        }
+}
+
+document.addEventListener('DOMContentLoaded', function() {
+        const allAccordion = document.querySelectorAll('.cs-accordion')
+        const windowMatcher = window.matchMedia("(max-width: 48rem)");
+
+        if (allAccordion.length === 0 || !windowMatcher.matches) return // return if there is no accordion parent 
+
+        document.querySelectorAll('button[data-trigger]').forEach(button => {
+                button.addEventListener('click', function() {
+                        // Toggle the 'accordion-show' class on the parent .cs-accordion
+                        const accordion = this.closest('.cs-accordion');
+                        accordion.classList.toggle('accordion-show');
+                });
+        });
+
+})
