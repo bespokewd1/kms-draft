51bdba0736ef1cce1733af54458a09a66502bc36
churchilldotexe <churchillexe@gmail.com>
2025-03-03 18:10:13 +0800

    chore: code stich logo change

--- src/assets/js/admin.js
+++ src/assets/js/admin.js
@@ -2,7 +2,7 @@ document.addEventListener("DOMContentLoaded", () => {
   const adminEl = document.getElementById("nc-root");
 
   const img = adminEl.querySelector("img");
-  img.setAttribute("src", "/assets/images/bespokelogo.png");
+  img.setAttribute("src", "{{ client.domain }}/assets/images/bespokelogo.png");
 
   const decapLogo = adminEl.querySelector("span.exus10f2");
   decapLogo.setAttribute("style", "display:none");
