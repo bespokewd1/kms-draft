e4c7626f179be567ed9d48e32b79390fbad685d6
churchilldotexe <churchillexe@gmail.com>
2025-03-03 18:02:12 +0800

    chore:change code stitch logo

--- src/assets/js/admin.js
+++ src/assets/js/admin.js
@@ -1,13 +1,9 @@
-document.addEventListener(
-  "DOMContentLoaded",
-  () => {
-    const adminEl = document.getElementById("nc-root");
+document.addEventListener("DOMContentLoaded", () => {
+  const adminEl = document.getElementById("nc-root");
 
-    const img = adminEl.querySelector(".css-u6jh90-CustomIconWrapper img");
-    img.setAttribute("src", "/assets/images/bespokelogo.png");
+  const img = adminEl.querySelector("img");
+  img.setAttribute("src", "/assets/images/bespokelogo.png");
 
-    const decapLogo = adminEl.querySelector("span.exus10f2");
-    decapLogo.setAttribute("style", "display:none");
-  },
-  { once: true },
-);
+  const decapLogo = adminEl.querySelector("span.exus10f2");
+  decapLogo.setAttribute("style", "display:none");
+});
